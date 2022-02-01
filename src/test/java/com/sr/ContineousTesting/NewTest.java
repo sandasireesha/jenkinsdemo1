0c6a566522a908e99d44f212c55adabda9decde8
sireesha <sanshasireesha@gmail>
2022-02-01 13:02:46 +0530

    done

--- src/test/java/com/sr/ContineousTesting/NewTest.java
+++ src/test/java/com/sr/ContineousTesting/NewTest.java
@@ -2,10 +2,10 @@ package com.sr.ContineousTesting;
 
 import static org.testng.Assert.assertEquals;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 
 import org.testng.annotations.BeforeClass;
